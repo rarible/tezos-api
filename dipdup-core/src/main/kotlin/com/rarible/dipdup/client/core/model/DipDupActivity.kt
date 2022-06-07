134103537f5fc1c3d809f84fdf9ccd24b0d97505
Roman Novikov <mystdeim@gmail.com>
2022-06-07 14:31:16 +0300

    feat: add activities for bids

--- dipdup-core/src/main/kotlin/com/rarible/dipdup/client/core/model/DipDupActivity.kt
+++ dipdup-core/src/main/kotlin/com/rarible/dipdup/client/core/model/DipDupActivity.kt
@@ -11,9 +11,18 @@ import java.time.OffsetDateTime
     JsonSubTypes.Type(value = DipDupOrderListActivity::class, name = DipDupActivity.LIST),
     JsonSubTypes.Type(value = DipDupOrderCancelActivity::class, name = DipDupActivity.CANCEL),
     JsonSubTypes.Type(value = DipDupOrderSellActivity::class, name = DipDupActivity.SELL),
+
     JsonSubTypes.Type(value = DipDupMintActivity::class, name = DipDupActivity.MINT),
     JsonSubTypes.Type(value = DipDupTransferActivity::class, name = DipDupActivity.TRANSFER),
-    JsonSubTypes.Type(value = DipDupBurnActivity::class, name = DipDupActivity.BURN)
+    JsonSubTypes.Type(value = DipDupBurnActivity::class, name = DipDupActivity.BURN),
+
+    JsonSubTypes.Type(value = DipDupMakeBidActivity::class, name = DipDupActivity.MAKE_BID),
+    JsonSubTypes.Type(value = DipDupGetBidActivity::class, name = DipDupActivity.GET_BID),
+    JsonSubTypes.Type(value = DipDupCancelBidActivity::class, name = DipDupActivity.CANCEL_BID),
+
+    JsonSubTypes.Type(value = DipDupMakeFloorBidActivity::class, name = DipDupActivity.MAKE_FLOOR_BID),
+    JsonSubTypes.Type(value = DipDupGetFloorBidActivity::class, name = DipDupActivity.GET_FLOOR_BID),
+    JsonSubTypes.Type(value = DipDupCancelFloorBidActivity::class, name = DipDupActivity.CANCEL_FLOOR_BID),
 )
 sealed class DipDupActivity {
     abstract val id: String
@@ -28,6 +37,14 @@ sealed class DipDupActivity {
         const val MINT = "MINT"
         const val TRANSFER = "TRANSFER"
         const val BURN = "BURN"
+
+        const val MAKE_BID = "MAKE_BID"
+        const val GET_BID = "GET_BID"
+        const val CANCEL_BID = "CANCEL_BID"
+
+        const val MAKE_FLOOR_BID = "MAKE_FLOOR_BID"
+        const val GET_FLOOR_BID = "GET_FLOOR_BID"
+        const val CANCEL_FLOOR_BID = "CANCEL_FLOOR_BID"
     }
 }
 
@@ -51,7 +68,7 @@ data class DipDupOrderCancelActivity(
     val source: TezosPlatform,
     val maker: String,
     val make: Asset,
-    val take: Asset,
+    val take: Asset
 ) : DipDupActivity()
 
 data class DipDupOrderSellActivity(
@@ -102,4 +119,76 @@ data class DipDupBurnActivity(
     val value: BigDecimal,
     val transactionId: String,
     val owner: String
+) : DipDupActivity()
+
+data class DipDupMakeBidActivity(
+    override val id: String,
+    override val date: OffsetDateTime,
+    override val reverted: Boolean,
+    val hash: String,
+    val source: TezosPlatform,
+    val maker: String,
+    val make: Asset,
+    val take: Asset,
+    val price: BigDecimal
+) : DipDupActivity()
+
+data class DipDupGetBidActivity(
+    override val id: String,
+    override val date: OffsetDateTime,
+    override val reverted: Boolean,
+    val hash: String,
+    val source: TezosPlatform,
+    val seller: String,
+    val nft: Asset,
+    val payment: Asset,
+    val buyer: String,
+    val price: BigDecimal
+) : DipDupActivity()
+
+data class DipDupCancelBidActivity(
+    override val id: String,
+    override val date: OffsetDateTime,
+    override val reverted: Boolean,
+    val hash: String,
+    val source: TezosPlatform,
+    val maker: String,
+    val make: Asset,
+    val take: Asset
+) : DipDupActivity()
+
+data class DipDupMakeFloorBidActivity(
+    override val id: String,
+    override val date: OffsetDateTime,
+    override val reverted: Boolean,
+    val hash: String,
+    val source: TezosPlatform,
+    val maker: String,
+    val make: Asset,
+    val take: Asset,
+    val price: BigDecimal
+) : DipDupActivity()
+
+data class DipDupGetFloorBidActivity(
+    override val id: String,
+    override val date: OffsetDateTime,
+    override val reverted: Boolean,
+    val hash: String,
+    val source: TezosPlatform,
+    val seller: String,
+    val nft: Asset,
+    val payment: Asset,
+    val buyer: String,
+    val price: BigDecimal
+) : DipDupActivity()
+
+data class DipDupCancelFloorBidActivity(
+    override val id: String,
+    override val date: OffsetDateTime,
+    override val reverted: Boolean,
+    val hash: String,
+    val source: TezosPlatform,
+    val maker: String,
+    val make: Asset,
+    val take: Asset
 ) : DipDupActivity()
