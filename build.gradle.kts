2371044cc7cb674fea84dddd4a22f5c247671d8c
Roman Novikov <mystdeim@gmail.com>
2022-08-05 14:08:10 +0300

    ECHO-205

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 subprojects {
     group = "com.rarible.protocol.tezos"
-    version = "1.0.83"
+    version = "1.0.84"
 
     apply(plugin = "com.rarible.protocol.tezos.common")
 }
--- dipdup-listener/src/main/kotlin/com/rarible/dipdup/listener/config/DipDupTopicProvider.kt
+++ dipdup-listener/src/main/kotlin/com/rarible/dipdup/listener/config/DipDupTopicProvider.kt
@@ -8,6 +8,16 @@ class DipDupTopicProvider {
         const val ORDER = "order_topic"
         const val ACTIVITY = "activity_topic"
         const val OWNERSHIP = "ownership_topic"
+
+        fun getActivityTopic(environment: String): String =
+            "protocol.$environment.tezos.indexer.activity"
+
+        fun getCollectionTopic(environment: String): String =
+            "protocol.$environment.tezos.indexer.collection"
+
+        fun getOrderTopic(environment: String): String =
+            "protocol.$environment.tezos.indexer.order"
+
     }
 
 }
